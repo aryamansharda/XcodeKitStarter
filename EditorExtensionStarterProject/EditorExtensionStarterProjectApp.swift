0f54af59bb0db6cca54ff1e21ddcb08f59c197ca
aryamansharda <asharda@turo.com>
2023-10-12 17:57:17 +0100

    Initial Commit

---
+++ EditorExtensionStarterProject/EditorExtensionStarterProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditorExtensionStarterProjectApp.swift
+//  EditorExtensionStarterProject
+//
+//  Created by Aryaman Sharda on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EditorExtensionStarterProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
